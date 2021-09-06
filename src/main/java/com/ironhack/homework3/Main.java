0cc0e262e4d2419236a851022e24fcea0e9fc019
Varvara Nechaeva <85786864+VarvaraNechaeva@users.noreply.github.com>
2021-09-06 15:47:11 +0200

    Make SpringBoot magic create tables in db

--- src/main/java/com/ironhack/homework3/Main.java
+++ src/main/java/com/ironhack/homework3/Main.java
@@ -1,10 +1,15 @@
 package com.ironhack.homework3;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args){
-        Menu.welcomeScreen();
-        Menu.mainMenu();
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+    Menu.welcomeScreen();
+    Menu.mainMenu();
+  }
 }
 
 

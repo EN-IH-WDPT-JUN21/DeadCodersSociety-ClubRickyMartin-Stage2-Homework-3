1430de867f4462e4808ac0201e00c9db5a1d3040
Varvara Nechaeva <85786864+VarvaraNechaeva@users.noreply.github.com>
2021-09-10 11:59:07 +0200

    make all tests work

--- src/test/java/ContactTest.java
+++ src/test/java/ContactTest.java
@@ -1,8 +1,6 @@
 import DeadCodersSocietyClubRickyMartinStage2Homework3.RestApplicationTest;
 import DeadCodersSocietyClubRickyMartinStage2Homework3.dao.Contact;
-import DeadCodersSocietyClubRickyMartinStage2Homework3.repository.AccountRepository;
 import DeadCodersSocietyClubRickyMartinStage2Homework3.repository.ContactRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
